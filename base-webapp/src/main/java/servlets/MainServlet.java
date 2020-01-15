4d8deb12b77d005bcd7526a08fa2dace24a9bc53
AcidSore <INFomicheva@yandex.ru>
2020-01-15 15:31:29 +0300

    no message

--- base-webapp/src/main/java/servlets/MainServlet.java
+++ base-webapp/src/main/java/servlets/MainServlet.java
@@ -2,63 +2,46 @@ package servlets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import persist.ProductRepository;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
-
+@WebServlet(name = "MainServlet", urlPatterns = "/")
 public class MainServlet extends HttpServlet {
-
-
     private static Logger logger = LoggerFactory.getLogger(MainServlet.class);
+    private ProductRepository productRepository;
 
-    private transient ServletConfig config;
-
-    // Метод вызывается контейнером после того как был создан класс сервлета
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        // Сохраняем полученную от сервера конфигурацию
-        this.config = config;
+    public void init() throws ServletException {
+        this.productRepository = (ProductRepository) getServletContext().getAttribute("productRepository");
+
+        if (productRepository == null) {
+            throw new ServletException("ProductRepository not created");
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("New GET request");
-        if (resp.getStatus()==404){
-            resp.getWriter().printf("<h1>Page not found </h1>");
+        try {
+            if (req.getServletPath().equals("/")) {
+                req.setAttribute("products", productRepository.findAll());
+                getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+//            } else if (req.getServletPath().equals("/create")) {
+//                showCreateProductPage(req, resp);
+//            } else if (req.getServletPath().equals("/edit")) {
+//                showEditProductPage(req, resp);
+//            } else if (req.getServletPath().equals("/delete")) {
+//                deleteProduct(req, resp);
+//            }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        if (resp.getStatus()==403){
-            resp.getWriter().printf("<h1>Forbidden page </h1>");
-        }
-        getServletContext().getRequestDispatcher("/main.html").forward(req, resp);
-    }
-
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return config;
-    }
-
-    // Метод вызывается для каждого нового HTTP запроса к данному сервлету
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        logger.info("New request");
-
-        // получаем объект типа BufferedWriter и пишем в него ответ на пришедший запрос
-        res.getWriter().println("<h1>Main Page</h1>");
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "BasicServlet";
-    }
-
-    // При завершении работы веб приложения, контейнер вызывает этот метод для всех сервлетов из этого приложения
-    @Override
-    public void destroy() {
-        logger.info("Servlet {} destroyed", getServletInfo());
     }
 }
--- base-webapp/src/main/webapp/index2.jsp
+++ base-webapp/src/main/webapp/index2.jsp
@@ -8,13 +8,34 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">EShop Catalog</a>
+    <a class="navbar-brand" href="#">EShop</a>
 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">List</a>
+            </li>
+        </ul>
+    </div>
 </nav>
+
+<div class="container">
+    <div class="row py-2">
         <div class="col-12">
-            <table class="table table-borderless">
+            <c:url value="/create" var="productCreateUrl"/>
+            <a class="btn btn-primary" href="${productCreateUrl}">Add Product</a>
+        </div>
+
+        <div class="col-12">
+            <table class="table table-bordered my-2">
                 <thead>
                 <tr>
+                    <th scope="col">Id</th>
                     <th scope="col">Name</th>
                     <th scope="col">Description</th>
                     <th scope="col">Price</th>
@@ -26,14 +47,19 @@
                 <%-- for (Product product : (List<Product>) request.getAttribute("products")) { --%>
                 <c:forEach var="product" items="${requestScope.products}">
                     <tr>
+                        <th scope="row"><c:out value="${product.id}"/></th>
                         <td><c:out value="${product.name}"/></td>
                         <td><c:out value="${product.description}"/></td>
                         <td><c:out value="${product.price}"/></td>
                         <td>
-                            <c:url value="/buy" var="productAddToCart">
+                            <c:url value="/edit" var="productEditUrl">
                                 <c:param name="id" value="${product.id}"/>
                             </c:url>
-                            <a class="btn btn-primary" href="${productAddToCart}">Buy</a>
+                            <a class="btn btn-success" href="${productEditUrl}"><i class="fas fa-edit"></i></a>
+                            <c:url value="/delete" var="productDeleteUrl">
+                                <c:param name="id" value="${product.id}"/>
+                            </c:url>
+                            <a class="btn btn-danger" href="${productDeleteUrl}"><i class="far fa-trash-alt"></i></a>
                         </td>
                     </tr>
                 </c:forEach>
@@ -41,13 +67,9 @@
                 </tbody>
             </table>
         </div>
-<div class="col-12">
-    <c:url value="/cart" var="productToCart"/>
-    <a class="btn btn-primary" href="${productToCart}">Go to cart</a>
-</div>
     </div>
 </div>
-</body>
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
